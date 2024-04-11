1be2ca253a7df56ac83d852116d2e1e987a34cc2
Dilipkumar <mail2Dilipu@gmail.com>
2024-04-11 09:49:35 +0530

    Test

---
+++ catalog-service/src/test/java/com/diliplabs/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diliplabs.bookstore.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
